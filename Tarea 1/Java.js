d2cf6e32f3ae8ba4f1b143347600ca3c3e5b2634
micmik-dot <michelitabanks@gmail.com>
2026-03-10 18:05:43 -0600

    Add files via upload

---
+++ Tarea 1/Java.js
@@ -0,0 +1,14 @@
+//Ejercicio 1/
+
+let number = 24
+let result = (number >= 10 && number <= 20) || (number >= 40 && number <= 60)
+
+console.log ("El calculo de rangos para el numero" + number + "es:" + result)
+
+//Ejercicio 2/
+
+let DogAge = 5
+let HumanAge = 2 * 10 (DogAge - 2) * 4
+
+
+console.log ("El perro de " + DogAge + "años tendria" + HumanAge + "si fuera un humano" )
